1e7ccd5f4df21d2b8c939325d238800438fa1f9e
Silvia795 <silviachan.13.23@gmail.com>
2026-04-10 16:27:19 -0700

    new scripts
    script for the camera, player, enemy, and damage made/moved over
    
    lesser note changes to the code for damage and enemies to be made to fit game mechanics

---
+++ Out-of-Light-Escape/Assets/Scripts/playerController.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreLayer;
+
+    [Range(1, 10)] [SerializeField] int HP;
+    [Range(3, 7)] [SerializeField] int speed;
+    [Range(2, 5)] [SerializeField] int sprintMod;
+    [Range(5, 25)] [SerializeField] int jumpSpeed;
+    [Range(1, 10)] [SerializeField] int jumpMax;
+    [Range(15, 50)] [SerializeField] int gravity;
+
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootRate;
+
+    int jumpCount;
+    int HPOrig;
+
+    float shootTimer; 
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HPOrig = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.yellow);
+
+        shootTimer += Time.deltaTime;
+
+        if(controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel.y = 0;
+
+        }
+
+        //moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+        
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+        playerVel.y -= gravity * Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
+            shoot();
+        }
+    }
+
+    void sprint()
+    {
+        if(Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if(Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            playerVel.y = jumpSpeed;
+            jumpCount++;
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if(dmg != null)
+            {
+                dmg.takeDamage(shootDamage);
+            }
+        }
+    }
+
+
+
+}
