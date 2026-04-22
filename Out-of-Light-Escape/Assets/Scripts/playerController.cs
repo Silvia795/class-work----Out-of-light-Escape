4b2ab6c60c2b6cebd96785326e11ccb7ac025311
11Infamous <tannerjjackson25@gmail.com>
2026-04-21 20:01:29 -0700

    Tanner 4/21 Lecture Update
    Added code from today's lecture, made sure everything was working. Moved player camera back since it was too far forward on the model causing clipping.

--- Out-of-Light-Escape/Assets/Scripts/playerController.cs
+++ Out-of-Light-Escape/Assets/Scripts/playerController.cs
@@ -1,9 +1,10 @@
 using System;
-using Unity.VisualScripting;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class playerController : MonoBehaviour, IDamage
+public class playerController : MonoBehaviour, IDamage, IPickup
 {
     [SerializeField] CharacterController controller;
     [Range(0, 100)] [SerializeField] int HP;
@@ -13,12 +14,11 @@ public class playerController : MonoBehaviour, IDamage
     [Range(1, 10)] [SerializeField] int jumpSpeed;
     [Range(1, 10)] [SerializeField] int jumpMax;
     [Range(1, 10)] [SerializeField] int gravity;
-    [Range(1, 10)] [SerializeField] int shootDamage;
-    [Range(1, 10)] [SerializeField] int shootDistance;
-    [Range(1, 10)] [SerializeField] float fireRate;
+    [SerializeField] GameObject gunModel;
+    [SerializeField] List<gunStats> gunList = new List<gunStats>();
     [SerializeField] ParticleSystem defaultHitEffect;
     [SerializeField] ParticleSystem enemyHitEffcet;
-
+    int gunListPos;
     int jumpCount;
     int HPOrig;
 
@@ -45,7 +45,7 @@ public class playerController : MonoBehaviour, IDamage
 
     void movement()
     {
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.white);
+        //Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.white);
         shootTimer += Time.deltaTime;
         if(controller.isGrounded)
         {
@@ -57,11 +57,11 @@ public class playerController : MonoBehaviour, IDamage
         jump();
         controller.Move(playerVel * Time.deltaTime);
         playerVel.y -= gravity * Time.deltaTime;
-
-        if(Input.GetButton("Fire1") && shootTimer >= fireRate && Time.timeScale >= 0)
+        if (Input.GetButton("Fire1") && gunList.Count > 0 && gunList[gunListPos].ammoCur > 0 && shootTimer >= gunList[gunListPos].shootRate)
         {
             shoot();
         }
+        selectGun();
     }
 
     void sprint()
@@ -88,19 +88,17 @@ public class playerController : MonoBehaviour, IDamage
     void shoot()
     {
         shootTimer = 0;
+        gunList[gunListPos].ammoCur--;
         RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDistance, ~ignoreLayer))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, gunList[gunListPos].shootDist, ~ignoreLayer))
         {
-            Debug.Log(hit.collider.name);
-            if (!hit.collider.CompareTag("Enemy"))
-            {
-                Instantiate(defaultHitEffect, hit.point, Quaternion.identity);
-            }
+            if (gunList[gunListPos] != null)
+                Instantiate(gunList[gunListPos].hitEffect, hit.point, Quaternion.identity);
 
-                IDamage dmg = hit.collider.GetComponent<IDamage>();
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
             if(dmg != null)
             {
-                dmg.takeDamage(shootDamage);
+                dmg.takeDamage(gunList[gunListPos].shootDamage);
             }
         }
     }
@@ -125,5 +123,36 @@ public class playerController : MonoBehaviour, IDamage
         gamemanager.instance.playerDamageFlash.SetActive(true);
         yield return new WaitForSecondsRealtime(0.1f);
         gamemanager.instance.playerDamageFlash.SetActive(false);
+    }
+    public void getGunStats(gunStats gun)
+    {
+        gunList.Add(gun);
+        gunListPos = gunList.Count - 1;
+        changeGun();
+
+
+
+
+
+
+    }
+    void changeGun()
+    {
+        gunModel.GetComponent<MeshFilter>().sharedMesh = gunList[gunListPos].gunModel.GetComponent<MeshFilter>().sharedMesh;
+        gunModel.GetComponent<MeshRenderer>().sharedMaterial = gunList[gunListPos].gunModel.GetComponent<MeshRenderer>().sharedMaterial;
+
+    }
+    void selectGun()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && gunListPos < gunList.Count - 1)
+        {
+            gunListPos++;
+            changeGun();
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && gunListPos > 0)
+        {
+            gunListPos--;
+            changeGun();
+        }
     }
 }
