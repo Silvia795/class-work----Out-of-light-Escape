b1c6c0e5c5691ccdcbefe72435b84c6f0c69e14c
David Deane <doge719@gmail.com>
2026-04-23 19:19:06 -0400

    Camera rotatio fixed, + player detection outline

---
+++ Out-of-Light-Escape/Assets/Scripts/SecurityCamera test code2!.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+
+public class SecurityCamera2 : MonoBehaviour
+{
+    // This code is a test code for the SecurityCamera model
+
+    [Range(5f, 90f)][SerializeField] float maxAngle = 90f;
+    [Range (10f, 30f)][SerializeField] float minTime = 10f;
+    [Range(10f, 30f)][SerializeField] float maxTime = 20f;
+    [Range(0.25f, 2f)][SerializeField] float detectionTimer;
+    [SerializeField] Transform cameraPivot; 
+
+    private bool playerDetected;
+    float playerDetectTimer;
+
+    Quaternion startRotation;
+    void Start()
+    {
+        startRotation = cameraPivot.localRotation;
+        StartCoroutine(SwivelRoutine());
+    }
+
+    void Update()
+    {
+        if(playerDetected)
+        {
+            playerDetectTimer += Time.deltaTime;
+        }
+    }
+
+    IEnumerator SwivelRoutine()
+    {
+        while (true)
+        {
+            yield return RotateTo(-maxAngle);
+            yield return new WaitForSeconds(0.5f);
+            yield return RotateTo(maxAngle);
+            yield return new WaitForSeconds(0.5f);
+
+        }
+    }
+
+IEnumerator RotateTo(float angle)
+    {
+        float duration = Random.Range(minTime, maxTime);
+        Quaternion startRot = cameraPivot.localRotation;
+        Quaternion targetRot = cameraPivot.localRotation * Quaternion.Euler(0, angle, 0);
+
+        float time = 0f;
+
+        while (time < duration)
+        {
+            cameraPivot.localRotation = Quaternion.Slerp(startRot, targetRot, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        cameraPivot.localRotation = targetRot;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerDetected = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerDetected =false;
+        }
+    }
+}
