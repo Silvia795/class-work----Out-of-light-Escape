2e747cec81966afe36d2f936f681ace97641bd04
Silvia795 <silviachan.13.23@gmail.com>
2026-05-16 11:39:43 -0700

    showcase level made and change to settings made

--- Out-of-Light-Escape/Assets/Scripts/SecurityCamera test code.cs
+++ Out-of-Light-Escape/Assets/Scripts/SecurityCamera test code.cs
@@ -1,65 +1,65 @@
-using System.Collections;
-using UnityEngine;
+//using System.Collections;
+//using UnityEngine;
 
-public class SecurityCamera : MonoBehaviour
-{
-    // This code is a test code for the SecurityCamera model
+//public class SecurityCamera : MonoBehaviour
+//{
+//    // This code is a test code for the SecurityCamera model
 
-    [Range(5f, 90f)][SerializeField] float maxAngle = 90f;
-    [Range (10f, 30f)][SerializeField] float minTime = 10f;
-    [Range(10f, 30f)][SerializeField] float maxTime = 20f;
+//    [Range(5f, 90f)][SerializeField] float maxAngle = 90f;
+//    [Range (10f, 30f)][SerializeField] float minTime = 10f;
+//    [Range(10f, 30f)][SerializeField] float maxTime = 20f;
 
-    Quaternion startRotation;
+//    Quaternion startRotation;
 
-    void Start()
-    {
-        startRotation = transform.rotation;
-        StartCoroutine(SwivelRoutine());
-    }
+//    void Start()
+//    {
+//        startRotation = transform.rotation;
+//        StartCoroutine(SwivelRoutine());
+//    }
 
-    void Update()
-    {
-        DetectPlayer();
-    }
+//    void Update()
+//    {
+//        DetectPlayer();
+//    }
 
-    void DetectPlayer()
-    {
-        RaycastHit hit;
+//    void DetectPlayer()
+//    {
+//        RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, detectionRange, playerLayer))
-        {
-            Debug.Log("Player Caught!");
-            gamemanager.instance.PlayerCaught();
-        }
-    }
+//        if (Physics.Raycast(transform.position, transform.forward, out hit, detectionRange, playerLayer))
+//        {
+//            Debug.Log("Player Caught!");
+//            gamemanager.instance.PlayerCaught();
+//        }
+//    }
 
 
-    IEnumerator SwivelRoutine()
-    {
-        while (true)
-        {
-            yield return RotateTo(-maxAngle);
-            yield return RotateTo(0);
-            yield return RotateTo(maxAngle);
-            yield return RotateTo(0);
-        }
-    }
+//    IEnumerator SwivelRoutine()
+//    {
+//        while (true)
+//        {
+//            yield return RotateTo(-maxAngle);
+//            yield return RotateTo(0);
+//            yield return RotateTo(maxAngle);
+//            yield return RotateTo(0);
+//        }
+//    }
 
-IEnumerator RotateTo(float angle)
-    {
-        float duration = Random.Range(minTime, maxTime);
-        Quaternion startRot = transform.rotation;
-        Quaternion targetRot = startRotation * Quaternion.Euler(0, angle, 0);
+//IEnumerator RotateTo(float angle)
+//    {
+//        float duration = Random.Range(minTime, maxTime);
+//        Quaternion startRot = transform.rotation;
+//        Quaternion targetRot = startRotation * Quaternion.Euler(0, angle, 0);
 
-        float time = 0f;
+//        float time = 0f;
 
-        while (time < duration)
-        {
-            transform.rotation = Quaternion.Slerp(startRot, targetRot, time / duration);
-            time += Time.deltaTime;
-            yield return null;
-        }
+//        while (time < duration)
+//        {
+//            transform.rotation = Quaternion.Slerp(startRot, targetRot, time / duration);
+//            time += Time.deltaTime;
+//            yield return null;
+//        }
 
-        transform.rotation = targetRot;
-    }
-}
+//        transform.rotation = targetRot;
+//    }
+//}
