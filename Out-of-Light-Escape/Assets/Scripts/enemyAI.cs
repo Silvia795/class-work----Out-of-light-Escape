a62f00e081c667ee50ea995b7406d0a54a3d5746
David Deane <doge719@gmail.com>
2026-05-21 00:07:37 -0400

    lighting fixes; performance increase

--- Out-of-Light-Escape/Assets/Scripts/enemyAI.cs
+++ Out-of-Light-Escape/Assets/Scripts/enemyAI.cs
@@ -34,7 +34,7 @@ public class enemyAI : MonoBehaviour, IDamage
 
     [Header("----- Detection Stats -----")]
 
-    [SerializeField] float detectionAmount;
+    [SerializeField] public float detectionAmount;
     [SerializeField] float detectionBuildSpeed = 0.5f;
     [SerializeField] float detectionLoseSpeed = 0.75f;
     [SerializeField] bool playerDetected;
