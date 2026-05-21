4424f36ee12862fc4fec4ef01c1137ef1fa9997f
David Deane <doge719@gmail.com>
2026-05-21 14:25:40 -0400

    Variant added

--- Out-of-Light-Escape/Assets/Scripts/RotateObjects.cs
+++ Out-of-Light-Escape/Assets/Scripts/RotateObjects.cs
@@ -2,12 +2,33 @@ using UnityEngine;
 
 public class RotateObjects : MonoBehaviour
 {
+    public enum RotDirection
+    {
+        X, Y, Z
+    }
+
     [SerializeField] Transform model;
     [SerializeField] int rotateSpeed;
+    [SerializeField] RotDirection rotateDirection;
 
     // Update is called once per frame
     void Update()
     {
-        model.transform.RotateAround(model.position, Vector3.forward, Time.deltaTime * rotateSpeed);
+        Vector3 axis = Vector3.up;
+
+        switch (rotateDirection)
+        {
+            case RotDirection.X:
+                axis = Vector3.right;
+                break;
+            case RotDirection.Y:
+                axis = Vector3.up;
+                break;
+            case RotDirection.Z:
+                axis = Vector3.forward;
+                break;
+        }
+
+        model.transform.RotateAround(model.position, axis, Time.deltaTime * rotateSpeed);
     }
 }
