47379560b0ee65e0a0c5edd0c0966d34257a20ae
LeeLee97 <2016leahthompson@gmail.com>
2026-05-21 19:14:43 -0500

    fixed detection code, added interaction with keycard

--- Out-of-Light-Escape/Assets/Scripts/keycardPickup.cs
+++ Out-of-Light-Escape/Assets/Scripts/keycardPickup.cs
@@ -3,18 +3,54 @@ using UnityEngine;
 public class keycardPickup : MonoBehaviour
 {
    [SerializeField] string keycardID = "Red";
+    [SerializeField] GameObject promptText;
+
+    bool playerInRange;
+    keycardInventory currentInventory;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            keycardInventory inventory = other.GetComponent<keycardInventory>();
+            playerInRange = true;
 
-            if (inventory != null)
+            currentInventory = other.GetComponent<keycardInventory>();
+
+            if (promptText != null)
             {
-                inventory.addKeycard(keycardID);
-                Destroy(gameObject);
+                promptText.SetActive(true);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+
+            if (promptText != null)
+            {
+                promptText.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentInventory != null)
+            {
+                currentInventory.addKeycard(keycardID);
+            }
+
+            if (promptText != null)
+            {
+                promptText.SetActive(false);
+            }
+
+            Destroy(gameObject);
         }
     }
 }
